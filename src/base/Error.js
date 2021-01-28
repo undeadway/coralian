5f256d4f0fc0552fbbcbf466147cf4b68ce45199
hzwaygc <hzwaygc@gmail.com>
2021-01-28 18:03:46 +0800

    修改懒加载的位置

--- src/base/Error.js
+++ src/base/Error.js
@@ -1,5 +1,8 @@
+/**
+ * 这里这么写的原因是 Error 和 common 之间存在关联调用
+ * 所以只能做一个懒加载模式
+ */
 let isNumber, getFunctionName;
-
 function init() {
 	if (!isNumber || !getFunctionName) {
 		let common = require("../base/common");
@@ -10,13 +13,14 @@ function init() {
 
 /* ==================== Error 的扩展 ==================== */
 function debugError(e) {
+	init();
+
 	alert("message:" + e.message);
 	alert("stack:" + e.stack);
 	throw e;
 }
 
 function errorCast(obj, type) {
-	init();
 
 	if (Function.TYPE_NAME !== typeof type) {
 		errorCast(type, Function);
@@ -31,7 +35,7 @@ function errorCast(obj, type) {
 Error.errorCast = errorCast;
 
 function indexOutOfBounds(given, want) {
-	init();
+
 	if (!isNumber(given)) errorCast(given, Number);
 	if (!isNumber(want)) errorCast(want, Number);
 
@@ -44,6 +48,7 @@ function indexOutOfBounds(given, want) {
 Error.indexOutOfBounds = indexOutOfBounds;
 
 function noReference(msg) {
+
 	let error = new ReferenceError();
 	error.message = msg || "当前引用错误。";
 	debugError(error);
@@ -51,6 +56,7 @@ function noReference(msg) {
 Error.noReference = noReference;
 
 function unsupportedType(type) {
+
 	let error = new TypeError();
 	error.message = typeOf(type) + "类型的数据不被当前操作所支持。";
 	debugError(error);
@@ -58,27 +64,32 @@ function unsupportedType(type) {
 Error.unsupportedType = unsupportedType;
 
 function unsupportedOperation(msg) {
+
 	msg += " 不被支持的操作";
 	debugError(new Error(msg));
 }
 Error.unsupportedOperation = unsupportedOperation;
 
 function noSuchMethod(name) {
+
 	debugError(new Error("方法 " + name + " 不存在。"));
 }
 Error.noSuchMethod = noSuchMethod;
 
 function noSuchProperty(name) {
+
 	debugError(new Error("属性 " + name + " 不存在。"));
 }
 Error.noSuchProperty = noSuchProperty;
 
 function errorStatement() {
+
 	debugError(new Error("语法错误，或被执行的逻辑不正确。"));
 }
 Error.errorStatement = errorStatement;
 
 function illegalArguments(msg) {
+
 	msg = msg || "函数参数不正确";
 	debugError(msg);
 }
