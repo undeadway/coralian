e43c7230ba2a6387e10ad694d68698e106f09cd7
waygc <hzwaygc@gmail.com>
2021-08-10 16:38:46 +0800

    结构化

--- test/index.js
+++ test/index.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 
 const modules = fs.readdirSync("./test/modules");
 
-for (let i = 0, len = modules.length; i < len; i++) {
+modules.map(module => {
 	try {
-		console.log("---------------" + modules[i]);
-		let module = require(`./modules/${modules[i]}`);
-		if (module.test) module.test();
+		console.log("---------------" + module);
+		let instance = require(`./modules/${module}`);
+		if (instance.test) instance.test();
 	} catch(e) {
 		console.log(e);
 	}
-}
+});
